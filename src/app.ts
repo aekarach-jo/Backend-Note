5d84f21e110e2d8468a75bfbb234f8e97f59ef30
aekarch.jo <aekarach.jomkhamsing@gmail.com>
2024-02-22 16:22:00 +0700

    aaaaaaaa

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 import noteRoutes from "./routes/notes";
 
 const app = express();
+app.get("/", (req, res) => { res.send("Express on Vercel"); }); const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 
 app.use(express.json());
 
@@ -14,10 +15,10 @@ app.use((req, res, next) => {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
-        console.error(error);
-        let errorMessage = "An unknow error occurred"
-        if (error instanceof Error) errorMessage = error.message;
-        res.status(500).json({ error: errorMessage });
+    console.error(error);
+    let errorMessage = "An unknow error occurred"
+    if (error instanceof Error) errorMessage = error.message;
+    res.status(500).json({ error: errorMessage });
 });
 
 export default app;
